3cf31aa61627dc9e0e7b0c96240ff0227e1e0940
bCoder778 <bCoder778@gmail.com>
2021-06-25 09:26:48 +0800

    Update version.go

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 var (
 	GitCommitLog = "unknown_unknown"
 	GitStatus    = "unknown_unknown"
-	Version      = "v0.3.0"
+	Version      = "v0.3.1"
 )
 
 func StringifySingleLine(appName string) string {
